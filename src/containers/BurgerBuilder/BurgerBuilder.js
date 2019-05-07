85b4589f3bb9e4c80e70aa3ee79417021c79e017
krsk <kacper.swiatek@icloud.com>
2019-05-07 21:25:07 +0200

    add global error handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = { //duże litery na wszystko bo to będzie globalna zmienna jakby
     salad: 0.5,
@@ -141,4 +142,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios); //withErrorHandler też oczekuje jako drugi props axiosa
