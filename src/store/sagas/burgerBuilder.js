4303100932f2b86c6fbeda7ac46c753ba08133a1
krsk <kacper.swiatek@icloud.com>
2019-07-17 23:53:47 +0200

    add saga to separate side effects from actions

---
+++ src/store/sagas/burgerBuilder.js
@@ -0,0 +1,12 @@
+import axios from '../../axios-orders';
+import { put } from 'redux-saga/effects';
+import * as actions from '../actions'; //index.js automatycznie
+
+export function* initIngredientsSaga(action) {
+    try {
+        const response = yield axios.get('https://react-burger-f0872.firebaseio.com/ingredients.json');
+        yield put(actions.setIngredients(response.data));
+    } catch (error) {
+        yield put(actions.fetchIngredientsFailed());
+    }
+}
